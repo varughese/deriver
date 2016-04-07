3ad911dd02e11b2b78724731495de56f8f2bc102
Mathew Varughese <mat@imginconline.com>
2016-04-06 20:40:45 -0400

    Start Term Parser

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
+var val;
+
+function parseTerm(term) {
+    var caret_pos = term.lastIndexOf("^");
+    var degree = term.substring(caret_pos+1);
+    term = term.substring(0, caret_pos);
+    var x_pos = term.indexOf("x");
+    if(term[term.length-1] !== ')') {
+        term = term.splice(term.indexOf("x"), '(');
+        term += ')';
+    }
+    var leftPar_pos = term.indexOf("(");
+    var rightPar_pos = term.lastIndexOf(")");
+    var value = term.substring(leftPar_pos+1, rightPar_pos);
+    var coefficent = Number(term.substring(0, leftPar_pos) || 1);
+    return {
+        coeff: coefficent,
+        value: value,
+        deg: degree
+    };
+}
+
+
+
 $(document).ready(function(){
   $("#submit").click(function(){
-    var val = $("#input").val();
-    console.log(val);
+    val = $("#input").val();
+    console.log(parseTerm(val));
   });
 });
+
+String.prototype.splice = function(start, newSubStr) {
+    return this.slice(0, start) + newSubStr + this.slice(start);
+};
