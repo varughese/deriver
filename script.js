fe5b86dc05b4d9fd7a9a80f3fc0f2366c9a1062f
BuildTools <j26walker@gmail.com>
2016-04-06 20:32:41 -0400

    Add input changer and track history

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+
 $(document).ready(function(){
   $("#submit").click(function(){
-    var val = $("#input").val();
+    var val = $("#input").val().replace(/-/g,"~");
     console.log(val);
+    $(".history").append("<div class='history-item'>"+val+"</div>");
   });
 });
