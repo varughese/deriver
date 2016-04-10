f8a7803ad14104e277a16462c96e554bc43a9ec3
Mathew Varughese <mat@imginconline.com>
2016-04-10 16:52:00 -0400

    Remove ParseTerm()

--- script.js
+++ script.js
@@ -1,25 +1,5 @@
 var val;
 
-function parseTerm(term) {
-    var caret_pos = term.lastIndexOf("^");
-    var degree = term.substring(caret_pos+1);
-    term = term.substring(0, caret_pos);
-    var x_pos = term.indexOf("x");
-    if(term[term.length-1] !== ')') {
-        term = term.splice(term.indexOf("x"), '(');
-        term += ')';
-    }
-    var leftPar_pos = term.indexOf("(");
-    var rightPar_pos = term.lastIndexOf(")");
-    var value = term.substring(leftPar_pos+1, rightPar_pos);
-    var coefficent = Number(term.substring(0, leftPar_pos) || 1);
-    return {
-        coeff: coefficent,
-        value: value,
-        deg: degree
-    };
-}
-
 function replaceNegative(){
     for(var i=0; i<val.length; i++){
         if(val.charAt(i)==='-' && (i===0 || "+*^/-".indexOf(val.charAt(i-1))>-1)){
