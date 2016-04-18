4daba5a1bb7f399a4f0f8f7307c2725c0849f716
Mathew Varughese <mat@imginconline.com>
2016-04-17 21:05:48 -0400

    Continue Basic unit test

--- test/parse.spec.js
+++ test/parse.spec.js
@@ -1,9 +1,59 @@
-describe('Parser Creates Correct Trees', function() {
-    it("Addition", function() {
-        var parsed = parseInput("2+4");
-        var tree = new Tree("+");
-        tree.l("2");
-        tree.r("4");
-        expect(parsed).toEqual(tree);
+describe('Parser::', function() {
+    describe('Basic:', function() {
+
+        it("Basic Addition", function() {
+            var parsed = parseInput("2+4");
+            var tree = new Tree("+");
+            tree.l("2");
+            tree.r("4");
+            expect(parsed).toEqual(tree);
+        });
+
+        it("Basic Multiplication", function() {
+            var parsed = parseInput("2*4");
+            var tree = new Tree("*");
+            tree.l("2");
+            tree.r("4");
+            expect(parsed).toEqual(tree);
+        });
+
+        describe("PEMDAS:", function() {
+
+            it("Add/Multiply", function() {
+                var parsed = parseInput("2+4*x");
+                var tree = new Tree("+");
+                var subTree = new Tree("*");
+                subTree.l("4");
+                subTree.r("x");
+                tree.l("2");
+                tree.r(subTree);
+                expect(parsed).toEqual(tree);
+            });
+
+            it("Exponents", function() {
+                var parsed = parseInput("4+4^x");
+                var tree = new Tree("+");
+                tree.l("4");
+                var subTree = new Tree("^");
+                subTree.l("4");
+                subTree.r("x");
+                tree.r(subTree);
+                expect(parsed).toEqual(tree);
+            });
+
+            it("Complicated Exponents", function() {
+                var parsed = parseInput("4^x^x^x");
+                var tree = new Tree("^");
+                tree.l("4");
+                var subTree = new Tree("^");
+                subTree.l("x");
+                var subSubTree = new Tree("^");
+                subSubTree.l("x");
+                subSubTree.r("x");
+                subTree.r(subSubTree);
+                tree.r(subTree);
+                expect(parsed).toEqual(tree);
+            });
+        });
     });
 });
