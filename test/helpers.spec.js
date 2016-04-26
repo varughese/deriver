24d0c32923632cb8bf2ef7db34d262bff63e425e
Mathew Varughese <mat@imginconline.com>
2016-04-25 23:21:31 -0400

    Unit test parseParens and cleanInput

---
+++ test/helpers.spec.js
@@ -0,0 +1,32 @@
+describe("Helpers::", function() {
+    describe("Parentheses Parser:", function() {
+
+        it("Simple", function() {
+            expect(parseParens("(3+4)+(4+4)")).toEqual([[0,4],[6,10]]);
+        });
+
+        it("Nested", function() {
+            expect(parseParens("(3+4+(3+x)+(3+4))+4")).toEqual([[0,16],[5,9],[11,15]]);
+        });
+
+    });
+
+    describe("Clean Input", function() {
+
+        it("Handle coefficents", function() {
+            expect(cleanInput("3x")).toBe("3*x");
+            expect(cleanInput("3x+4x")).toBe("3*x+4*x");
+        });
+
+        it("Number then Parentheses", function() {
+            expect(cleanInput("3(4)")).toBe("3*(4)");
+            expect(cleanInput("3(4+x)")).toBe("3*(4+x)");
+            // expect(cleanInput("x(9+x)")).toBe("x*(9+x)");
+        });
+
+        it("Parentheses then Parentheses", function() {
+            expect(cleanInput("(3+x)(3+x)")).toBe("(3+x)*(3+x)");
+            expect(cleanInput("(3+x)(3+x)(6+x)")).toBe("(3+x)*(3+x)*(6+x)");
+        });
+    });
+});
