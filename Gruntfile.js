ab715ed264462228b4f431300daa87c550f947b7
Jason Walker <j26walker@gmail.com>
2016-05-20 14:09:20 +0000

    Install grunt

---
+++ Gruntfile.js
@@ -0,0 +1,20 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      build: {
+        src: 'app/parser/deriver.js',
+        dest: 'build/<%= pkg.name %>.min.js'
+      }
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  // Default task(s).
+  grunt.registerTask('default', ['uglify']);
+
+};
