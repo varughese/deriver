7f1e160a402060c00a9079a6c2f7a1b019916307
Mathew Varughese <mat@imginconline.com>
2016-05-22 15:44:46 -0400

    Simplifier multiplies

--- src/simplifier.js
+++ src/simplifier.js
@@ -1,5 +1,5 @@
 function simplify(tree) {
-    var __schemas = [parseInput('###*(###/$$$)')];
+    var __schemas = [parseInput('###*(###/$$$)'), parseInput('###*(###*$$$)')];
     for(var s in __schemas) {
         if(tree.equals(__schemas[s])) {
             return fns[s](tree);
@@ -11,6 +11,13 @@ var fns = [
     function(tree) {
         var res = new Tree("/");
 
+        res.l(tree.left.val * tree.right.left.val);
+        res.r(tree.right.right);
+
+        return res;
+    },
+    function(tree) {
+        var res = new Tree("*");
         res.l(tree.left.val * tree.right.left.val);
         res.r(tree.right.right);
 
