fceacd8eb5043c18d4904e8ebd92f1c55c5e227f
Mathew Varughese <mat@imginconline.com>
2016-05-29 18:19:50 -0400

    Grunt concats deriver from sibling folder

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,25 +1,39 @@
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    copy: {
-        js: {
-            files: [
-                {src: './bower_components/jquery/dist/jquery.min.js', dest: 'lib/js/jquery.min.js'}
-            ]
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        concat: {
+            options: {
+                process: function(src, filepath) {
+                    return '// Source: ' + filepath + '\n' + src;
+                },
+            },
+            dist: {
+                src: ['../deriver-master/src/*.js'],
+                dest: 'lib/js/deriver.js'
+            }
         },
-        css: {
-            files: [
-                {src: './bower_components/bootstrap/dist/css/bootstrap.css', dest: 'lib/css/bootstrap.css'}
-            ]
+        copy: {
+            js: {
+                files: [{
+                    src: './bower_components/jquery/dist/jquery.min.js',
+                    dest: 'lib/js/jquery.min.js'
+                }]
+            },
+            css: {
+                files: [{
+                    src: './bower_components/bootstrap/dist/css/bootstrap.css',
+                    dest: 'lib/css/bootstrap.css'
+                }]
+            }
         }
-    }
-  });
+    });
 
-  // Load the plugin that provides the "copy" task.
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  // Default task(s).
-  grunt.registerTask('default', ['copy']);
+    // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    // Default task(s).
+    grunt.registerTask('default', ['concat', 'copy']);
 
 };
